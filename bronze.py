388c453532416214efae8bf77f7ed991d781b975
lessing-anderson <lessing-anderson>
2025-01-26 02:17:52 +0000

    Added access to raw container

--- bronze.py
+++ bronze.py
@@ -14,3 +14,27 @@
 
 # MAGIC %sql
 # MAGIC select * from bronze.teste_nytaxi.teste
+
+# COMMAND ----------
+
+#https://<your-workspace>.azuredatabricks.net/#secrets/createScope
+
+dbutils.secrets.listScopes()
+
+# COMMAND ----------
+
+dbutils.secrets.list("akv")
+
+
+# COMMAND ----------
+
+spark.conf.set(
+    "fs.azure.account.key.dlhpikadev.dfs.core.windows.net",
+    dbutils.secrets.get(scope="akv", key="secretAccessKeyDataLake")
+)
+
+# COMMAND ----------
+
+df = spark.read.format("json").load("abfss://raw@dlhpikadev.dfs.core.windows.net/IGDB/games/dcac6327-3add-5a38-9f3e-ec3954219476/")
+
+df.display()
